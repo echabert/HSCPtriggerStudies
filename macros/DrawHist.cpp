c88f7f12a0c17988855184023579fc195a5d00be
raph <raphael0803@msn.com>
2021-05-06 17:08:39 +0200

    sum of both

--- macros/DrawHist.cpp
+++ macros/DrawHist.cpp
@@ -60,6 +60,8 @@ void DrawHist::FitSignalBg(){
 	
 	TH1D* HIST_FIT = (TH1D*) HIST_MASSES->Clone();
 
+	TH1D* HIST_FITSUM = (TH1D*) HIST_MASSES->Clone();
+
 	//HIST_FITBG->SetMarkerColor(kBlack);
 	//HIST_FITBG->SetMarkerColor(kBlack);
 	//HIST_FITSIG->SetMarkerColor(kBlack);
@@ -70,16 +72,21 @@ void DrawHist::FitSignalBg(){
 	HIST_FITSIG->SetName("FitSignal");
 
 	HIST_FIT->SetName("General fit");
+
+	HIST_FITSUM->SetName("Fit with sum");
+
+	HIST_FITSUM->GetXaxis()->SetTitle("Mass [GeV]");
+	HIST_FITSUM->GetYaxis()->SetTitle(" # candidates");
 	
-	HIST_FITBG->GetXaxis()->SetTitle("M [GeV]");
+	HIST_FITBG->GetXaxis()->SetTitle("Mass [GeV]");
 	HIST_FITBG->GetYaxis()->SetTitle(" # candidates");
 	
 
-	HIST_FITSIG->GetXaxis()->SetTitle("M [GeV]");
+	HIST_FITSIG->GetXaxis()->SetTitle("Mass [GeV]");
 	HIST_FITSIG->GetYaxis()->SetTitle(" # candidates");
 	
 
-	HIST_FIT->GetXaxis()->SetTitle("M [GeV]");
+	HIST_FIT->GetXaxis()->SetTitle("Mass [GeV]");
 	HIST_FIT->GetYaxis()->SetTitle(" # candidates");
 	
 	int nbinxfit = HIST_FITSIG->GetNbinsX();
@@ -109,7 +116,7 @@ void DrawHist::FitSignalBg(){
 
 	for(int x = 40 ; x < 50 ; x++){
 		int randombg = rand()%(ValBins[valsize-1]-ValBins[0]+1)+ValBins[0];
-		cout << randombg << endl;
+		//cout << randombg << endl;
 		nbentriesZ+=HIST_FITSIG->GetBinContent(x);
 		//HIST_FITBG->SetBinContent(x,0);
 		HIST_FITBG->SetBinContent(x,randombg);
@@ -128,26 +135,11 @@ void DrawHist::FitSignalBg(){
 	pad->Draw();
 	pad->cd();
 
-
-
-	HIST_FITSIG->Fit("gaus");
-	
-	TF1 *s = (TF1*)HIST_FITSIG->GetListOfFunctions()->FindObject("gaus");
-	
-	//double m = s->GetParameter(0);
-	double IntegralGauss = s->Integral(86,96);
-
-
-	HIST_FITBG->Fit("pol0");
-	TF1 *g = (TF1*)HIST_FITBG->GetListOfFunctions()->FindObject("pol0");
-
-
-	//double c = g->GetParameter(0);
-
-	double IntegralBg = g->Integral(86,96);
+	int sizeparamgaus = 3,sizeparampol=2;
+	double m[sizeparamgaus],m2[sizeparampol];
 	
 
-	TF1 *g2 = new TF1("g2", "pol0", 15,140);
+	TF1 *g2 = new TF1("g2", "pol1", 15,140);
 	g2->SetLineColor(6);
 	g2->SetLineStyle(1);
 	g2->SetLineWidth(2);
@@ -157,19 +149,42 @@ void DrawHist::FitSignalBg(){
 	g1->SetLineColor(kRed);
 	g1->SetLineStyle(1);
 	g1->SetLineWidth(3);
+
 	
+	HIST_FITBG->Fit(g2,"R");
+	HIST_FITSIG->Fit(g1,"R");
+
+
 	HIST_FIT->Fit(g2,"R");
 	HIST_FIT->Fit(g1,"R+");
 
-	auto leg1 =new TLegend(0.13,0.45,0.48,0.65);
 
-
+	TF1 *g = (TF1*)HIST_FIT->GetListOfFunctions()->FindObject(g2);
+	double IntegralBg = g2->Integral(86,96);
+	TF1 *s = (TF1*)HIST_FIT->GetListOfFunctions()->FindObject(g1);
+	double IntegralGauss = g1->Integral(86,96);
+	
+	for (int i =0 ; i < sizeparamgaus ; i++){
+		m[i] = g1->GetParameter(i);
+		cout << m[i] << endl;
+	}
+	for (int i =0 ; i < sizeparampol ; i++){
+		m2[i] = g2->GetParameter(i);
+		cout << m2[i] << endl;
+	}
+	
 	
 
+	TF1 *fitboth = new TF1("fits","gaus(0) +pol1(3)", 80,100);
+	fitboth->SetLineColor(2);
+	fitboth->SetParameters(m[0],m[1],m[2],m2[0],m2[1]);
+	HIST_FITSUM->Fit(fitboth,"R+");
 
+	auto leg1 =new TLegend(0.13,0.45,0.48,0.65);
+	
 	leg1->SetHeader("Background and signal fits");
-	leg1->AddEntry(g2,"Background : constant","l");
-	leg1->AddEntry(g1,"Signal : gaussian","l");
+	leg1->AddEntry(fitboth,"Sum of background and signal in the region of interest","l");
+	//leg1->AddEntry(g1,"Signal : gaussian","l");
 	//leg1->SetLegendFont(42);
 	leg1->SetTextFont(42);
 	leg1->SetTextSize(0.03);
@@ -181,11 +196,25 @@ void DrawHist::FitSignalBg(){
 	header->SetTextColor(1);
 	header->SetTextSize(.04);
 	
+
+
+	/*auto c2 = new TCanvas("c2","Fit for background",1300,700);
+	auto leg2 = new TLegend(0.13,0.45,0.48,0.65);
+	leg1->AddEntry(g2,"Background : polynom of degree 1","l");
+	leg2->SetTextFont(42);
+	leg2->SetTextSize(0.03);
+	leg2->SetBorderSize(0);
+	leg2->SetFillStyle(0);
+	c2->cd();
 	
+	leg2->Draw();
+	TLegendEntry *header2 = (TLegendEntry*)leg2->GetListOfPrimitives()->First();
+	header2->SetTextColor(1);
+	header2->SetTextSize(.04);*/
 
 
-	cout << "This is the integral of the Signal for mass between [86-96]: " << IntegralGauss << endl;
-	cout << "This is the integral of the background for mass between [86-96] = 0 : " << IntegralBg << endl;
+	cout << "Integral of the Signal for mass between [86-96] GeV: " << IntegralGauss << endl;
+	cout << "Integral of the background for mass between [86-96] GeV = 0 : " << IntegralBg << endl;
 	cout << "Ratio signal/total ¦ bins in 40-50 -> 0 = " << (IntegralGauss *1.0 / (IntegralGauss+IntegralBg))*100 << " %" << endl;
 	
 	cout << "There was " << nbentriesZ << " Z pair" << endl;
@@ -199,10 +228,11 @@ void DrawHist::FitSignalBg(){
 	InfosPurity << "There was " << nbentriesZ << " Z pair" << endl;
 
 	InfosPurity.close();
+
 	OutputHisto->cd();
+
 	c1->Write();
-	
-	
+	HIST_FITSUM->Write();
 	HIST_FIT->Write();
 	HIST_FITBG->Write();
 	HIST_FITSIG->Write();
