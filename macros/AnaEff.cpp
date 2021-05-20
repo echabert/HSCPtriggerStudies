0ff3006d1b0ebb793c7b8ae4cf49e2cd5ad37d96
raph <raphael0803@msn.com>
2021-05-20 10:12:04 +0200

    enrichi W

--- macros/AnaEff.cpp
+++ macros/AnaEff.cpp
@@ -377,8 +377,8 @@ double AnaEff::deltaR(double delta) {
 double AnaEff::MuonsMissingET(){
 	vector< pair<float, int > > muonPT,muonPHI,muonETA;
 	double MissingET,mu_phi,mu_eta,mu_pt,mu_px,mu_py,mu_pz;
-	TLorentzVector sum,transf;
-	PxPyPzM4D mu;
+	TLorentzVector mu,sum,transf;
+	PxPyPzMVector mu;
 	vector<double> missingET;
 	// indications eric : pz = 0, pas d'infos sur pz,px,py ? muon_p / muon_pt peut donner px ?
 	
