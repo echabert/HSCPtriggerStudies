abba939dc4d2e5a4a98039b1a90abf751a86bac2
raph <raphael0803@msn.com>
2021-05-25 19:16:41 +0200

    test stop2000

--- macros/AnaEff.cpp
+++ macros/AnaEff.cpp
@@ -264,9 +264,9 @@ void AnaEff::Loop()
 			}
 			
 			passedevent+=1;
-			cout << "Before fill" << endl;
+			//cout << "Before fill" << endl;
 			trigEff_selection_obs.FillNoMap2(PosPass,HighestPT,1);
-			cout << "After fill" << endl;
+			//cout << "After fill" << endl;
 			//trigEff_selection_obs.FillNoMap(vtrigger,HighestPT,1);
 			//trigEff_presel.FillNoMap(vtrigger,HighestMET);					
 		}
