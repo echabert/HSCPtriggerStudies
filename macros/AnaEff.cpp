efe2c02dc0ac3540a02cd40924ded842d7d84825
raph <raphael0803@msn.com>
2021-05-25 19:13:52 +0200

    test stop2000

--- macros/AnaEff.cpp
+++ macros/AnaEff.cpp
@@ -445,7 +445,7 @@ double AnaEff::MuonsMissingET(){
 		}
 		
 		cout << "after muon pt cut" << endl;
-
+		cout << muonPT.size() << muonETA.size() << muonPHI.size() << muonP.size()<<endl;
 
 		sort(muonPT.begin(),muonPT.end());
 		int index = muonPT[muonPT.size()-1].second;
