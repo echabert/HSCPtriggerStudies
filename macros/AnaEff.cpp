68a4a7061ce4f4c48318a762d26d7343cd5d5551
raph <raphael0803@msn.com>
2021-05-26 16:54:30 +0200

    gluino2000

--- macros/AnaEff.cpp
+++ macros/AnaEff.cpp
@@ -66,8 +66,8 @@ void AnaEff::Loop()
 
 	string TransferDistribZ = "DistribZpeak";
 	string TransferDistribW = "DistribWpeak";
-	string DataType = "Gluino1600";
-	float TheorMass = 1600;
+	string DataType = "Gluino2000";
+	float TheorMass = 2000;
 	string NameCompleteListTest = "ListeInteretTriggers";
 
 	string ListAllTriggers = ListAll + DataType + ExtTxt;
