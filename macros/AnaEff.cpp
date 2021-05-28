7d58dcc7abec7f9084aac879fcc07cb0cc0f84a3
raph <raphael0803@msn.com>
2021-05-28 19:12:02 +0200

    a

--- macros/AnaEff.cpp
+++ macros/AnaEff.cpp
@@ -395,9 +395,9 @@ int AnaEff::Selection(){
 		if(hscp_iso2_tk[ihs] >= 100){ //50
 			yon=false;
 		}
-		if(track_ias_ampl[hscp_track_idx[ihs]] < 0.1){ //50
+		/*if(track_ias_ampl[hscp_track_idx[ihs]] < 0.1){ //50
 			yon=false;
-		}
+		}*/
 
 
 		/*if (muon_isMediumMuon[ihs]){
