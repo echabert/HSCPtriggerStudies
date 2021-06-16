4adfe70607e70629feafbff0ea59e5f5ff29af75
raph <raphael0803@msn.com>
2021-06-16 20:08:38 +0200

    a

--- macros/AnaEff.cpp
+++ macros/AnaEff.cpp
@@ -41,7 +41,7 @@ const double uncertaintyMu = 0.0000000024;
 const double massW = 80.379;
 const double uncertaintyW = 0.012;
 
-const float TheorMass = 1800;
+const float TheorMass = 1600;
 
 void AnaEff::Loop()
 {
@@ -70,7 +70,7 @@ void AnaEff::Loop()
 
 	string TransferDistribZ = "DistribZpeak";
 	string TransferDistribW = "DistribWpeak";
-	string DataType = "Gluino18002021";
+	string DataType = "Gluino1600";
 	
 	string NameCompleteListTest = "ListeInteretTriggers";
 
