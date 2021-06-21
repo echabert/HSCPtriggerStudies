7556da189eb2ae87acb53396f9c0fb6d27c05964
raph <raphael0803@msn.com>
2021-06-21 18:01:26 +0200

    a

--- macros/AnaEff.cpp
+++ macros/AnaEff.cpp
@@ -702,23 +702,23 @@ void AnaEff::AssoGenId(){
 			nbmothgen+=1;
 		}
 		if(gen_pdg[i] == 1009213 || gen_pdg[i] == 1009323 || gen_pdg[i] == 1092214 || gen_pdg[i] == 1091114 || gen_pdg[i] == 1092224 || gen_pdg[i] == 1093114 || gen_pdg[i] == 1093224 || gen_pdg[i] == 1093314 || gen_pdg[i] == 1093334 || gen_pdg[i] == 1000612 || gen_pdg[i] == 1000632 || gen_pdg[i] == 1000652 || gen_pdg[i] == 1006211 || gen_pdg[i] == 1006213 || gen_pdg[i] == 1006223 || gen_pdg[i] == 1006313 || gen_pdg[i] == 1006321 || gen_pdg[i] == 1006323){ // rajouter tout ici
-			cout << gen_moth_pdg[i] << endl;
-			if(gen_moth_pdg[i] == 1000021){
+			//cout << gen_moth_pdg[i] << endl;
+			//if(gen_moth_pdg[i] == 1000021){
 				 cout << gen_pdg[i] << " , " << gen_moth_pdg[i] << " , " << gen_status[i] << endl;
 				 candidatesrh.push_back(i);
 
-			}
+			//}
 			
 
 		}
 
 
 		if(gen_pdg[i] == 1000622 || gen_pdg[i] == 1093324 || gen_pdg[i] == 1092114 || gen_pdg[i] == 1000993 || gen_pdg[i] == 1009113 || gen_pdg[i] == 1009223 || gen_pdg[i] == 1009313 || gen_pdg[i] == 1009333 || gen_pdg[i] == 1093214 || gen_pdg[i] == 1000642 || gen_pdg[i] == 1006113 || gen_pdg[i] == 1006311 || gen_pdg[i] == 1006313) {
-			if(gen_moth_pdg[i] == 1000021){
+			//if(gen_moth_pdg[i] == 1000021){
 				 cout << gen_pdg[i] << " , " << gen_moth_pdg[i] << " , " << gen_status[i] << endl;
 				 candidatesneutral.push_back(i);
 
-			}
+			//}
 			
 			
 		}
@@ -744,32 +744,49 @@ void AnaEff::AssoGenId(){
 
 	nbtot+=1;
 
-	if(candidatesrh.size() >= 2 && candidatesneutral.size() == 0 ){
+	if( candidatesrh.size() >= 1 && candidatesneutral.size() >= 1 ){
 
 		nbchch+=1;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 	}
 
-	if(candidatesrh.size() >= 1 && candidatesneutral.size() >= 1 ){
+	if(candidatesrh.size() >= 2 && candidatesneutral.size() == 0){
 		//cout << "charged + neutral " << endl;
 		
 		
-		cout << "neutral + charged " << endl;
+		cout << "charged + charged " << endl;
 		for(int i = 0; i < ntracks ; i++){
 			//for(int j=0; j< candidates.size() ; j++){
 			
 			double deltatranfr1 = deltaR2(track_eta[i], track_phi[i], gen_eta[candidatesrh[candidatesrh.size()-1]], gen_phi[candidatesrh[candidatesrh.size()-1]]);
 			double finaldelta1 = deltaR(deltatranfr1);
 
+			double deltatranfr2 = deltaR2(track_eta[i], track_phi[i], gen_eta[candidatesrh[candidatesrh.size()-2]], gen_phi[candidatesrh[candidatesrh.size()-2]]);
+			double finaldelta2 = deltaR(deltatranfr2);			
 
-			double deltatranfr2 = deltaR2(track_eta[i], track_phi[i], gen_eta[candidatesneutral[candidatesneutral.size()-1]], gen_phi[candidatesneutral[candidatesneutral.size()-1]]);
-			double finaldelta2 = deltaR(deltatranfr2);
+
+
+			/*double deltatranfr2 = deltaR2(track_eta[i], track_phi[i], gen_eta[candidatesneutral[candidatesneutral.size()-1]], gen_phi[candidatesneutral[candidatesneutral.size()-1]]);
+			double finaldelta2 = deltaR(deltatranfr2);*/
 
 			//cout << finaldelta << endl;
 			if (finaldelta1 < 0.3){
 				alo = true;
 				nbchn+=1;
-				cout << "Track number " << i << " is associated with charged gluino " << candidatesrh[candidatesrh.size()-1] << endl;
+				cout << "Track number " << i << " is associated with charged gluino 1 " << candidatesrh[candidatesrh.size()-1] << endl;
 				poverm1 = ((gen_pt[candidatesrh[candidatesrh.size()-1]] * cosh(gen_eta[candidatesrh[candidatesrh.size()-1]]))/TheorMass);
 				DISTRIB_POVERMASSO1->Fill(poverm1);
 				Psurm1 = poverm1;
@@ -782,7 +799,7 @@ void AnaEff::AssoGenId(){
 			if (finaldelta2 < 0.3){
 				alo2 = true;
 				nbchn+=1;
-				cout << "Track number " << i << " is associated with neutral gluino " << candidatesneutral[candidatesneutral.size()-1] << endl;
+				cout << "Track number " << i << " is associated with charged gluino 2" << candidatesneutral[candidatesneutral.size()-1] << endl;
 				poverm2 = ((gen_pt[candidatesrh[candidatesrh.size()-2]] * cosh(gen_eta[candidatesrh[candidatesrh.size()-2]]))/TheorMass);
 				DISTRIB_POVERMASSO1->Fill(poverm2);
 				Psurm2 = poverm2;
