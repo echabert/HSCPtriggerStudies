0d0aa373343677ffdbbe1424f8ec13e828f971eb
raph <raphael0803@msn.com>
2021-06-22 12:40:35 +0200

    a

--- macros/AnaEff.cpp
+++ macros/AnaEff.cpp
@@ -697,8 +697,9 @@ void AnaEff::AssoGenId(){
 	int nbnn=0;
 	double p1=0,p2=0,eta1=0,eta2=0,pt1=0,pt2=0;
 	float poverm1,poverm2;
+	cout << " NEW EVENT ------"<<endl;
 	for(int i=0; i < ngenpart ; i++){
-		cout << " NEW EVENT ------"<<endl;
+		
 		cout << "gen : " << gen_pdg[i] << " , gen_moth : " << gen_moth_pdg[i] << " , status : " << gen_status[i] << " , p = pt * cosh(eta) : " << gen_pt[i] * cosh(gen_eta[i]) << endl;
 		if(gen_moth_pdg[i] == 1000021){
 			nbmothgen+=1;
